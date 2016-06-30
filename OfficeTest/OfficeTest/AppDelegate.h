13b44b349b03bd9512376067af632f4d111f1a72
nian <nian@tusmdeMacBook-Pro-3.local>
2016-06-30 11:08:35 +0800

    all file

---
+++ OfficeTest/OfficeTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OfficeTest
+//
+//  Created by suruikeji on 16/6/7.
+//  Copyright © 2016年 com.SuRuikeji.fany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
